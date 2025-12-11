5c8f3c0d0f0bb10dab6673bf438625a2a47ba5d9
Kateryna Babakova <kateryna.babakova@gmail.com>
2025-12-11 14:24:39 -0600

    Fix indentation for return statement in max_average_subarray

--- sliding_window/max_average_subarray.py
+++ sliding_window/max_average_subarray.py
@@ -17,5 +17,5 @@ def max_average(nums, k):
     for i in range(k, len(nums)):
         window_sum += nums[i] - nums[i - k]
         max_sum = max(max_sum, window_sum)
-        return max_sum / k
+    return max_sum / k
 
